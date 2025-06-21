a87889682c9664c963f1a14c4f4e5764fa6ee173
Pablo Arteaga <dj.pablo.ac@gmail.com>
2025-06-21 13:20:25 -0500

    feat: add Tailwind CSS support and configure Vite
    - Added Tailwind CSS and its Vite plugin to package.json
    - Imported Tailwind CSS styles in index.css
    - Updated main.tsx to include index.css
    - Configured Vite to use Tailwind CSS plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig((mode) => {
@@ -11,6 +12,9 @@ export default defineConfig((mode) => {
       port: parseInt(PORT),
       host: true,
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
   }
 })
